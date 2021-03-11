c576046b04714d87c65f43a9c59202e43b575e83
Lars Wiegers <lars@altios.nl>
2021-03-11 21:12:54 +0100

    Add in a default user acc for myself

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+Use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Lars Wiegers',
+            'email' => 'larswiegers@live.nl',
+            'password' => Hash::make('password'),
+        ]);
     }
 }
